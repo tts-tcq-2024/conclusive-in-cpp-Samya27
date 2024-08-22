81b76f2b9c0b7a4e6c25c1da787ebe1cd9045d61
Samya Sharma <68061663+Samya27@users.noreply.github.com>
2024-08-23 02:07:06 +0530

    Update typewise-alert.h

--- typewise-alert.h
+++ typewise-alert.h
@@ -1,6 +1,20 @@
-#include "Identify_Temp_Breach.h"
-
+//#include "Identify_Temp_Breach.h"
 #pragma once
+
+typedef enum {
+  PASSIVE_COOLING,
+  HI_ACTIVE_COOLING,
+  MED_ACTIVE_COOLING
+} CoolingType;
+
+typedef enum {
+  NORMAL,
+  TOO_LOW,
+  TOO_HIGH
+} BreachType;
+
+BreachType inferBreach(double value, double lowerLimit, double upperLimit);
+BreachType classifyTemperatureBreach(CoolingType coolingType, double temperatureInC);
 
 typedef enum {
   TO_CONTROLLER,
