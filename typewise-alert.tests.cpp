6804b478308cdb8084ae9e8aaeb41f6d762ed60c
Samya Sharma <68061663+Samya27@users.noreply.github.com>
2024-08-23 07:23:12 +0530

    Update typewise-alert.tests.cpp

--- typewise-alert.tests.cpp
+++ typewise-alert.tests.cpp
@@ -1,8 +1,16 @@
 #include <gtest/gtest.h>
 #include "typewise-alert.h"
 
-TEST(TypeWiseAlertTestSuite,InfersBreachAccordingToLimits) {
+TEST(TypeWiseAlertTestSuite, TestClassifiesTemperatureBreach) {
+    EXPECT_EQ(classifyTemperatureBreach(PASSIVE_COOLING, 25), NORMAL);
+    EXPECT_EQ(classifyTemperatureBreach(PASSIVE_COOLING, 50), TOO_HIGH);
+    EXPECT_EQ(classifyTemperatureBreach(HI_ACTIVE_COOLING, 60), TOO_HIGH);
+    EXPECT_EQ(classifyTemperatureBreach(HI_ACTIVE_COOLING, 35), NORMAL);
+    EXPECT_EQ(classifyTemperatureBreach(MED_ACTIVE_COOLING, -1), TOO_LOW);
+    EXPECT_EQ(classifyTemperatureBreach(MED_ACTIVE_COOLING, 20), NORMAL);
+}
+TEST(TypeWiseAlertTestSuite,TestInfersBreachAccordingToLimits) {
     EXPECT_EQ(inferBreach(14, 20, 40), TOO_LOW);
-    EXPECT_EQ(inferBreach(30, 20, 40), TOO_LOW);
+    EXPECT_EQ(inferBreach(30, 20, 40), NORMAL);
     EXPECT_EQ(inferBreach(50, 20, 40), TOO_HIGH);
 }
