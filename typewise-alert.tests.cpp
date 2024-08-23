e8a39033a09bac6c8cc4ad014fb964efa9690e6d
Samya Sharma <68061663+Samya27@users.noreply.github.com>
2024-08-23 07:34:26 +0530

    Update typewise-alert.tests.cpp

--- typewise-alert.tests.cpp
+++ typewise-alert.tests.cpp
@@ -14,3 +14,20 @@ TEST(TypeWiseAlertTestSuite,TestInfersBreachAccordingToLimits) {
     EXPECT_EQ(inferBreach(30, 20, 40), NORMAL);
     EXPECT_EQ(inferBreach(50, 20, 40), TOO_HIGH);
 }
+ 
+TEST(TypeWiseAlertTestSuite, CheckAndAlert_Low_Breach_Email) {
+    BatteryCharacter batteryChar = {PASSIVE_COOLING, "Test_Brand"};
+    checkAndAlert(TO_EMAIL, batteryChar, 5);  // Should send too low alert via email
+}
+TEST(TypeWiseAlertTestSuite, CheckAndAlert_High_Breach_Email) {
+    BatteryCharacter batteryChar = {PASSIVE_COOLING, "Test_Brand"};
+    checkAndAlert(TO_EMAIL, batteryChar, 50);  // Should send too high alert via email
+}
+TEST(TypeWiseAlertTestSuite, CheckAndAlertController) {
+    BatteryCharacter batteryChar = {PASSIVE_COOLING, "Test_Brand"};
+    checkAndAlert(TO_CONTROLLER, batteryChar, 25);  // Should send normal alert to controller
+}
+TEST(TypeWiseAlertTestSuite, CheckAndAlertEmail) {
+    BatteryCharacter batteryChar = {HI_ACTIVE_COOLING, "Test_Brand"};
+    checkAndAlert(TO_EMAIL, batteryChar, 60);  // Should send too high alert via email
+}
